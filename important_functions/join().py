dc7871b50029e07a84a923b9e81da7f38fd327ee
sciencethebird <sciencethebird@gmail.com>
2018-10-23 11:39:20 +0800

    Update important_functions/join().py

---
+++ important_functions/join().py
@@ -0,0 +1,11 @@
+
+
+# join list element with certain character
+
+s1 = "-"
+s2 = ""
+seq = ("r", "u", "n", "o", "o", "b") # 字符串序列
+list = ["lol","lmao","haha"]
+print (s1.join( seq ))
+print (s2.join( seq ))
+print ("-".join(list))
