8ac23ad9e64c9bd26a8d7ae7794d3e0ddeb4f5f9
Birb <34442036+Sciencethebird@users.noreply.github.com>
2018-11-10 17:22:29 +0800

    update
    File_Practice.py update
    with)as.py

---
+++ Basic_Concepts/File/File_Practice.py
@@ -0,0 +1,52 @@
+import os
+
+cwd = os.getcwd()
+print('Current Working Directory:\t' + cwd)
+
+lt = [1,2,3,4]
+temp = str(lt)
+
+def what_is_in_file(filename = 'default.txt'):
+    with open(cwd+filename, 'r') as file:
+        return file.read()
+
+#'w' mode
+#create a new file and delete the old one(if there's any)
+#discard whatever was in the file 
+#Write from begining
+
+print('\nw mode')
+file = open(cwd + 'default.txt', 'w')
+file.write('123456789')
+file.close()
+print(what_is_in_file()+'\n\n')
+
+#'r+' mode
+# read/write from beginning (keep old data)
+# '+' basically means it modifies the original file, and allowing read/write
+# can't run if the the file DNE 
+
+print('r+ mode, add 000')
+print('before change:' + what_is_in_file())
+file = open(cwd + 'default.txt', 'r+')
+file.write('000')
+file.close()
+print('after change: ' + what_is_in_file()+'\n\n')
+
+
+#'a+' mode
+#read/write from the end
+# add to the end, for both read/write
+print('a+ mode, add xxx')
+print('before change:' + what_is_in_file())
+file = open(cwd + 'default.txt', 'a+')
+file.write('xxx')
+file.close()
+print('after change: ' + what_is_in_file()+'\n\n')
+
+#readlines returns a list
+file = open(cwd + 'default.txt','r')
+text = file.readlines()
+print(text[0])
+file.close()
+
---
+++ Basic_Concepts/with_as.py
@@ -0,0 +1,15 @@
+
+
+
+zero = 0
+a = 1
+b = 10
+
+def divide(a, b):
+    return a/b
+  
+#file open example
+with  open('demo.txt', 'r+') as file:
+    file.write('xdxdxd')
+    temp = file.read()
+    print(temp)
