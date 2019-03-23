80a5c9c6d3e968158ef8c0c4b4395f5ccbd24d25
Birb <34442036+Sciencethebird@users.noreply.github.com>
2019-03-23 18:36:29 +0800

    update example

--- Basic_Concepts/Dictionary.py
+++ Basic_Concepts/Dictionary.py
@@ -1,9 +1,19 @@
+
 acquaintance = {'Yahan': 'My junior high crush', 'Heng Li':'my religious brother', 'Robert':'My college roommate'}
 print(acquaintance)
 
-print(acquaintance['Yahan'])
+print("Use key to dereference: ", acquaintance['Yahan'])
+
+print("\nValues in dictionary")
 for k in acquaintance:
     print(k)
 
+print()
+# keys / values
+print('{:8}'.format('keys'), acquaintance.keys())
+print('{:8}'.format('values') ,acquaintance.values())
+print()
+
+# items()
 for k, v in acquaintance.items():
-    print(k, v)
+    print("Key: {0:8}, Value: {1}".format(k,v))
