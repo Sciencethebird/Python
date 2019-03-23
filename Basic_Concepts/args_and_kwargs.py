9e0500d6c27dbbdbd307f2a1b390840a65f092c8
Birb <34442036+Sciencethebird@users.noreply.github.com>
2019-03-23 16:59:12 +0800

    new example

--- Basic_Concepts/args_and_kwargs.py
+++ Basic_Concepts/args_and_kwargs.py
@@ -1,7 +1,7 @@
 
 a = ['birb','doggo','seeb']
 c = ['birb','doggo','seebdddd']
-
+d = {'d1':a, 'd2':c} #dictionary
 def foo(*args, **kwargs):
     for idx, element in enumerate(args):
         print('args ', idx, args[idx])
@@ -14,6 +14,14 @@ def foo(*args, **kwargs):
 
 foo(a, c, d = a, s = c) # a, c are args
                         # d, s are kwargs
+
+#if you want to pass in *args directly
+print()
+foo(*a)
+
+#if you want to pass in **kwargs directly
+print()
+foo(**d)
 '''
 args  0 ['birb', 'doggo', 'seeb']
 args  1 ['birb', 'doggo', 'seebdddd']
@@ -22,5 +30,17 @@ args  ['birb', 'doggo', 'seebdddd']
 Keyword  d
 kwargs  seeb
 Keyword  s
+kwargs  seebdddd
+
+args  0 birb
+args  1 doggo
+args  2 seeb
+args  birb
+args  doggo
+args  seeb
+
+Keyword  d1
+kwargs  seeb
+Keyword  d2
 kwargs  seebdddd
 '''
